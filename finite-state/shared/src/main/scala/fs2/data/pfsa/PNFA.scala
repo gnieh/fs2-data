cd4968ed8eed0a2383d206a95a9d1b78cc2ba000
Yannick Heiber <yannick.heiber@mobimeo.com>
2022-10-02 15:13:29 +0200

    Re-add collection compat imports for 2.12

--- finite-state/shared/src/main/scala/fs2/data/pfsa/PNFA.scala
+++ finite-state/shared/src/main/scala/fs2/data/pfsa/PNFA.scala
@@ -25,6 +25,8 @@ import cats.syntax.all._
 import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuilder
 
+import scala.collection.compat._
+
 private[data] class PNFA[P, T](val init: Int, val finals: Set[Int], val transitions: Map[Int, List[(Option[P], Int)]])(
     implicit P: Pred[P, T]) {
 
