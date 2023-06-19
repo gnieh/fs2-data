c913da4c7a8bf0374880d19845904866f95d0173
Lucas Satabin <lucas.satabin@gnieh.org>
2023-06-19 20:08:53 +0200

    Make CI happy

--- json/src/test/scala/fs2/data/json/LegacyStringCharLikeChunk.scala
+++ json/src/test/scala/fs2/data/json/LegacyStringCharLikeChunk.scala
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2023 Lucas Satabin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package fs2
 package data
 package json
 
+import scala.annotation.nowarn
+
 import text.CharLikeChunks
 
+@nowarn
 class LegacyStringCharLikeChunk[F[_]] extends CharLikeChunks[F, String] {
   class StringContext(var string: Array[Char], var idx: Int, var rest: Stream[F, String])
 
