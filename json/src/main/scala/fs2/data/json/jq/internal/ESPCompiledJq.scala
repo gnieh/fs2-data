96f5bf3c0fb469a8e2324b34c9ee974e4de2911c
Lucas Satabin <lucas.satabin@gnieh.org>
2023-10-02 17:29:06 +0200

    Remove unused class

--- json/src/main/scala/fs2/data/json/jq/internal/ESPCompiledJq.scala
+++ json/src/main/scala/fs2/data/json/jq/internal/ESPCompiledJq.scala
@@ -34,11 +34,6 @@ private[jq] class ESPCompiledJq[F[_]: RaiseThrowable](val esp: JqESP[F]) extends
 
   def andThen(that: CompiledJq[F]): CompiledJq[F] =
     that match {
-      // case that: ESPCompiledJq[F] =>
-      //  // fuse them to avoid tagging/untagging between each stage
-      //  new FusedESPCompiledJq[F](NonEmptyChain(this.esp, that.esp))
-      // case that: FusedESPCompiledJq[F] =>
-      //  new FusedESPCompiledJq[F](this.esp +: that.esps)
       case that: PipedCompiledJq[F] =>
         new PipedCompiledJq[F](this +: that.jqs)
       case _ =>
