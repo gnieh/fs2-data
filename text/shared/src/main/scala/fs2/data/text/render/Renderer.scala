338be653f4c5dddcdf522298ba9400cb3503455d
Lucas Satabin <lucas.satabin@gnieh.org>
2024-01-30 23:44:33 +0100

    Add basic util to split big texts

--- text/shared/src/main/scala/fs2/data/text/render/Renderer.scala
+++ text/shared/src/main/scala/fs2/data/text/render/Renderer.scala
@@ -16,9 +16,29 @@
 
 package fs2.data.text.render
 
-import fs2.{Stream, Pure}
+import fs2.{Chunk, Pure, Stream}
 
 trait Renderer[Event] {
+
+  /**
+    * Splits words in the given text into a stream of document events.
+    * This is a utility method, when you want to reformat a text using
+    * the pretty printer. The pretty printing algorithm assumes that each
+    * `DocEvent.Text` is an atomic value, so if it contains new lines it
+    * can break the computations.
+    *
+    * Between 2 words, it adds a `DocEvent.Line` event. Empty lines are not
+    * Generated.
+    *
+    * @param text The text to split
+    * @param wordBoundary The regular expression on which to split words
+    */
+  def words(text: String, wordBoundary: String = raw"\s+"): Stream[Pure, DocEvent] =
+    Stream
+      .chunk(Chunk.array(text.split(wordBoundary)))
+      .filter(_.nonEmpty)
+      .map(DocEvent.Text(_))
+      .intersperse(DocEvent.Line)
 
   /** Transforms the event into a stream of document events.
     * The stream may be partial (e.g. opening a group when the event describes a new tree node).
