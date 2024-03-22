4e1b84f78ab1a0269c38129084d76edd69b466b0
satabin <satabin@users.noreply.github.com>
2024-03-22 21:52:26 +0000

    deploy: 84b65a49afcf53a04486b1eb263b1e221751e490

--- cookbooks/scripts/jsonlines.scala
+++ cookbooks/scripts/jsonlines.scala
@@ -1,5 +1,5 @@
 //> using toolkit typelevel:default
-//> using dep org.gnieh::fs2-data-json-circe::1.10.0
+//> using dep org.gnieh::fs2-data-json-circe::1.11.0
 
 import cats.effect.{ExitCode, IO, IOApp}
 import com.monovore.decline.{Command, Opts}
