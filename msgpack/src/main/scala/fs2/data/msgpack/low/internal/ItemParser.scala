9509c30e15ad11637f706e8eff870310c1356d9e
Mariusz Jakoniuk <jarmuszz@tuta.io>
2024-06-11 22:46:55 +0200

    Add back one level of nesting in msgpack `parseItem`
    Reverts c9f2dff as it broke the `@switch` annotatiion.

--- msgpack/src/main/scala/fs2/data/msgpack/low/internal/ItemParser.scala
+++ msgpack/src/main/scala/fs2/data/msgpack/low/internal/ItemParser.scala
@@ -68,33 +68,39 @@ private[low] object ItemParser {
         case Headers.Map16     => parseMap(2, ctx)
         case Headers.Map32     => parseMap(4, ctx)
 
-        // Positive fixint
-        case x if ((x & 0x80) == 0) =>
-          Pull.pure(ctx.prepend(MsgpackItem.SignedInt(ByteVector(byte))))
-
-        // fixmap
-        case x if ((x & 0xf0) == 0x80) =>
-          val length = byte & 0x0f // 0x8f- 0x80
-          Pull.pure(ctx.prepend(MsgpackItem.Map(length)))
-
-        // fixarray
-        case x if ((x & 0xf0) == 0x90) =>
-          val length = byte & 0x0f // 0x9f- 0x90
-          Pull.pure(ctx.prepend(MsgpackItem.Array(length)))
-
-        // fixstr
-        case x if ((x & 0xe0) == 0xa0) =>
-          val length = byte & 0x1f
-          requireBytes(length, ctx) map { res =>
-            res.accumulate(MsgpackItem.Str(_))
+        case _ => {
+          // Positive fixint
+          if ((byte & 0x80) == 0) {
+            Pull.pure(ctx.prepend(MsgpackItem.SignedInt(ByteVector(byte))))
           }
 
-        // Negatve fixint
-        case x if ((x & 0xe0) == 0xe0) =>
-          Pull.pure(ctx.prepend(MsgpackItem.SignedInt(ByteVector(byte))))
+          // fixmap
+          else if ((byte & 0xf0) == 0x80) {
+            val length = byte & 0x0f // 0x8f- 0x80
+            Pull.pure(ctx.prepend(MsgpackItem.Map(length)))
+          }
 
-        case _ =>
-          Pull.raiseError(new MsgpackParsingException(s"Invalid type ${byte}"))
+          // fixarray
+          else if ((byte & 0xf0) == 0x90) {
+            val length = byte & 0x0f // 0x9f- 0x90
+            Pull.pure(ctx.prepend(MsgpackItem.Array(length)))
+          }
+
+          // fixstr
+          else if ((byte & 0xe0) == 0xa0) {
+            val length = byte & 0x1f
+            requireBytes(length, ctx) map { res =>
+              res.accumulate(MsgpackItem.Str(_))
+            }
+          }
+
+          // Negatve fixint
+          else if ((byte & 0xe0) == 0xe0) {
+            Pull.pure(ctx.prepend(MsgpackItem.SignedInt(ByteVector(byte))))
+          } else {
+            Pull.raiseError(new MsgpackParsingException(s"Invalid type ${byte}"))
+          }
+        }
       }
     }
   }
