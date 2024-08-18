b73258346c80bbfe178cf6a0838fd4e5e9ce22fe
Mariusz Jakoniuk <jarmuszz@tuta.io>
2024-08-18 22:00:54 +0200

    Reformat ValidationSpec.scala

--- msgpack/src/test/scala/fs2/data/msgpack/ValidationSpec.scala
+++ msgpack/src/test/scala/fs2/data/msgpack/ValidationSpec.scala
@@ -58,46 +58,60 @@ object ValidationSpec extends SimpleIOSuite {
       .compile
       .foldMonoid
 
-
   test("should raise if integer values exceed 64 bits") {
     validation1(
       MsgpackItem.UnsignedInt(hex"10000000000000000") -> new ValidationErrorAt(0, "Unsigned int exceeds 64 bits"),
-      MsgpackItem.SignedInt(hex"10000000000000000")-> new ValidationErrorAt(0, "Signed int exceeds 64 bits")
+      MsgpackItem.SignedInt(hex"10000000000000000") -> new ValidationErrorAt(0, "Signed int exceeds 64 bits")
     )
   }
 
   test("should raise if string or binary values exceed 2^32 - 1 bytes") {
     validation1(
-      MsgpackItem.Str(ByteVector.empty.padLeft(Math.pow(2, 32).toLong)) -> new ValidationErrorAt(0, "String exceeds (2^32)-1 bytes"),
-      MsgpackItem.Bin(ByteVector.empty.padLeft(Math.pow(2, 32).toLong)) -> new ValidationErrorAt(0, "Bin exceeds (2^32)-1 bytes"),
+      MsgpackItem.Str(ByteVector.empty.padLeft(Math.pow(2, 32).toLong)) -> new ValidationErrorAt(
+        0,
+        "String exceeds (2^32)-1 bytes"),
+      MsgpackItem.Bin(ByteVector.empty.padLeft(Math.pow(2, 32).toLong)) -> new ValidationErrorAt(
+        0,
+        "Bin exceeds (2^32)-1 bytes")
     )
   }
 
   test("should raise on unexpected end of input") {
     validation(
       List(MsgpackItem.Array(2), MsgpackItem.True) -> new ValidationError("Unexpected end of input (starting at 0)"),
-      List(MsgpackItem.Array(2), MsgpackItem.Array(1), MsgpackItem.True) -> new ValidationError("Unexpected end of input (starting at 0)"),
-      List(MsgpackItem.Array(1), MsgpackItem.Array(1)) -> new ValidationError("Unexpected end of input (starting at 1)"),
-      List(MsgpackItem.Array(0), MsgpackItem.Array(1)) -> new ValidationError("Unexpected end of input (starting at 1)"),
-
+      List(MsgpackItem.Array(2), MsgpackItem.Array(1), MsgpackItem.True) -> new ValidationError(
+        "Unexpected end of input (starting at 0)"),
+      List(MsgpackItem.Array(1), MsgpackItem.Array(1)) -> new ValidationError(
+        "Unexpected end of input (starting at 1)"),
+      List(MsgpackItem.Array(0), MsgpackItem.Array(1)) -> new ValidationError(
+        "Unexpected end of input (starting at 1)"),
       List(MsgpackItem.Map(1), MsgpackItem.True) -> new ValidationError("Unexpected end of input (starting at 0)"),
-      List(MsgpackItem.Map(1), MsgpackItem.Map(1), MsgpackItem.True, MsgpackItem.True) -> new ValidationError("Unexpected end of input (starting at 0)"),
-      List(MsgpackItem.Map(2), MsgpackItem.True, MsgpackItem.Map(1)) -> new ValidationError("Unexpected end of input (starting at 2)"),
-      List(MsgpackItem.Map(2), MsgpackItem.True, MsgpackItem.Map(1)) -> new ValidationError("Unexpected end of input (starting at 2)"),
-      List(MsgpackItem.Map(0), MsgpackItem.Map(1)) -> new ValidationError("Unexpected end of input (starting at 1)"),
+      List(MsgpackItem.Map(1), MsgpackItem.Map(1), MsgpackItem.True, MsgpackItem.True) -> new ValidationError(
+        "Unexpected end of input (starting at 0)"),
+      List(MsgpackItem.Map(2), MsgpackItem.True, MsgpackItem.Map(1)) -> new ValidationError(
+        "Unexpected end of input (starting at 2)"),
+      List(MsgpackItem.Map(2), MsgpackItem.True, MsgpackItem.Map(1)) -> new ValidationError(
+        "Unexpected end of input (starting at 2)"),
+      List(MsgpackItem.Map(0), MsgpackItem.Map(1)) -> new ValidationError("Unexpected end of input (starting at 1)")
     )
   }
 
   test("should raise if extension data exceeds 2^32 - 1 bytes") {
     validation1(
-      MsgpackItem.Extension(0x54, ByteVector.empty.padLeft(Math.pow(2, 32).toLong)) -> new ValidationErrorAt(0, "Extension data exceeds (2^32)-1 bytes"),
+      MsgpackItem.Extension(0x54, ByteVector.empty.padLeft(Math.pow(2, 32).toLong)) -> new ValidationErrorAt(
+        0,
+        "Extension data exceeds (2^32)-1 bytes")
     )
   }
 
   test("should raise if nanoseconds fields exceed 999999999") {
     validation1(
-      MsgpackItem.Timestamp64(0xEE6B280000000000L)-> new ValidationErrorAt(0, "Timestamp64 nanoseconds cannot be larger than '999999999'"),
-      MsgpackItem.Timestamp96(1000000000, 0)-> new ValidationErrorAt(0, "Timestamp96 nanoseconds cannot be larger than '999999999'"),
+      MsgpackItem.Timestamp64(0xee6b280000000000L) -> new ValidationErrorAt(
+        0,
+        "Timestamp64 nanoseconds cannot be larger than '999999999'"),
+      MsgpackItem.Timestamp96(1000000000, 0) -> new ValidationErrorAt(
+        0,
+        "Timestamp96 nanoseconds cannot be larger than '999999999'")
     )
   }
 }
