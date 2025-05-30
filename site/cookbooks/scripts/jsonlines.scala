5435cd984bd5c8630528681b0a4cf6ec47fffc4a
Scala Steward <scala_steward@virtuslab.com>
2025-05-30 14:32:44 +0000

    Update fs2-data-json-circe to 1.12.0

--- site/cookbooks/scripts/jsonlines.scala
+++ site/cookbooks/scripts/jsonlines.scala
@@ -1,5 +1,5 @@
 //> using toolkit typelevel:default
-//> using dep org.gnieh::fs2-data-json-circe::1.11.3
+//> using dep org.gnieh::fs2-data-json-circe::1.12.0
 
 import cats.effect.{ExitCode, IO, IOApp}
 import com.monovore.decline.{Command, Opts}
