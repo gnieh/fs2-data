e350c8f6a8cfc96f1236d86737a1e154ef7e858b
Yannick Heiber <ybasket42@googlemail.com>
2026-04-04 20:24:08 +0200

    Fix CBOR encoding of JSON numbers with 3, 5, 6 or 7 bytes
    CBOR only allows power-of-2 length for numbers, so we need to pad accordingly.

---
+++ cbor-json/shared/src/test/scala/fs2/data/cbor/RoundtripSpec.scala
@@ -0,0 +1,31 @@
+package fs2
+package data
+
+import cats.effect.IO
+import fs2.data.cbor.high.CborValue
+import weaver.SimpleIOSuite
+
+object RoundtripSpec extends SimpleIOSuite {
+
+  test("CBOR items should round-trip through JSON") {
+    val items = List(
+      CborValue.Integer(123456), // 3 bytes
+      CborValue.Integer(1235213352876L), // 5 bytes
+      CborValue.TextString("Hello, world!"),
+      CborValue.True,
+      CborValue.False,
+      CborValue.Null
+    )
+
+    Stream
+      .emits(items)
+      .through(cbor.high.toItems)
+      .through(cbor.json.decodeItems[IO])
+      .through(json.cbor.encodeItems)
+      .through(cbor.high.parseValues)
+      .compile
+      .toList
+      .map { result => expect.same(items, result) }
+  }
+
+}
